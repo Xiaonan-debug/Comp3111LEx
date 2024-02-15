596f37100ef84d4c24b443279ca34664cc801736
Xiaonan LUO <xluobd@s-MacBook-Pro-4.local>
2024-02-15 12:32:11 +0800

    add two lines, first time

--- src/main/java/Lab1/mainApp.java
+++ src/main/java/Lab1/mainApp.java
@@ -4,8 +4,10 @@ public class mainApp {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
+        System.out.println("Welcome to computer");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b, n));
         System.out.println(n+" ! = "+myLibrary.factorial(n));
         System.out.println("Program ended");
+        System.out.println("practice of git commands");
     }
 }
